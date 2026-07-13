24f43f117513969734c4c41130b320d09f7bb7f1
srikary12 <121927567+srikary12@users.noreply.github.com>
2026-07-13 10:47:42 +0530

    Add: longest-substring-without-duplicates - submission-6

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-6.go
@@ -0,0 +1,14 @@
+func lengthOfLongestSubstring(s string) int {
+	l, r := 0, 0
+	res := 0
+	charSet := make(map[byte]bool)
+	for r = 0; r<len(s);r++ {
+		for charSet[s[r]] {
+			delete(charSet, s[l])
+            l++
+		}
+		charSet[s[r]] = true
+		res = max(res, r-l+1)
+	}
+	return res
+}
